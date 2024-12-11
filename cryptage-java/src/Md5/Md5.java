885290a225fbdc01602849d77ff8dee1a9158b0c
VictorSulivan <matheoprofessionnel12@gmail.com>
2024-12-11 10:25:19 +0100

    correct indentation

--- cryptage-java/src/Md5/Md5.java
+++ cryptage-java/src/Md5/Md5.java
@@ -5,10 +5,8 @@ import java.security.NoSuchAlgorithmException;
 
 public class Md5 {
     //this function returns a hash of a message
-    public static String createHashWithMd5(String input)
-    {
-        try
-        {
+    public static String createHashWithMd5(String input) {
+        try {
             //static getInstance() method is called with hashing MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
             //calculating message digest of an input that return array of byte
@@ -17,8 +15,7 @@ public class Md5 {
             BigInteger no = new BigInteger(1, messageDigest);
             //converting message digest into hex value
             StringBuilder hashtext = new StringBuilder(no.toString(16));
-            while (hashtext.length() < 32)
-            {
+            while (hashtext.length() < 32) {
                 //if hashtext does not contain 32 characters, complete them with the missing 0s.
                 hashtext.insert(0, "0");
             }
@@ -26,8 +23,7 @@ public class Md5 {
             return hashtext.toString();
         }
         //for specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e)
-        {
+        catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
